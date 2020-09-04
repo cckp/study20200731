f32eb40809e0e17cdbdb4e7a2069c7309b309a00
lijiasheng <lijiasheng@sinobest.cn>
2020-09-04 17:40:08 +0800

    【增加】N皇后问题和LeetCode752问题（广度优先搜索）

--- study/src/main/java/com/study/ljs/leetcode/Q752.java
+++ study/src/main/java/com/study/ljs/leetcode/Q752.java
@@ -12,11 +12,14 @@ public class Q752 {
     public static void main(String[] args) {
         Q752 q = new Q752();
         System.out.println(q.openLock(new String[]{"1111", "1234", "7654", "9876"}, "1100"));
+        System.out.println(q.openLock2(new String[]{"1111", "1234", "7654", "9876"}, "1100"));
+
+
     }
 
-    public int openLock(String[] deadends, String target){
+    public int openLock(String[] deadends, String target) {
         Set<String> deads = new HashSet<>();
-        for (int i = 0; i <deadends.length ; i++) {
+        for (int i = 0; i < deadends.length; i++) {
             deads.add(deadends[i]);
         }
         Set<String> visited = new HashSet<>();
@@ -25,25 +28,25 @@ public class Q752 {
         queue.offer("0000");
         visited.add("0000");
         int step = 0;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            for (int i = 0; i <size ; i++) {
+            for (int i = 0; i < size; i++) {
                 String cur = queue.poll();
 
-                if (deads.contains(cur)){
+                if (deads.contains(cur)) {
                     continue;
                 }
-                if (cur.equals(target)){
+                if (cur.equals(target)) {
                     return step;
                 }
                 for (int j = 0; j < 4; j++) {
-                    String up = upOne(cur,j);
-                    if (!visited.contains(up)){
+                    String up = upOne(cur, j);
+                    if (!visited.contains(up)) {
                         queue.offer(up);
                         visited.add(up);
                     }
-                    String down = downOne(cur,j);
-                    if (!visited.contains(down)){
+                    String down = downOne(cur, j);
+                    if (!visited.contains(down)) {
                         queue.offer(down);
                         visited.add(down);
                     }
@@ -54,23 +57,74 @@ public class Q752 {
         return -1;
     }
 
-    private String upOne(String str,int i){
+    private String upOne(String str, int i) {
         char[] chars = str.toCharArray();
-        if (chars[i]=='9'){
-            chars[i]='0';
-        }else {
+        if (chars[i] == '9') {
+            chars[i] = '0';
+        } else {
             chars[i]++;
         }
         return new String(chars);
     }
-    private String downOne(String str,int i){
-        if (i>=str.length()) System.out.println("i:"+i+"  str:"+str);
+
+    private String downOne(String str, int i) {
+        if (i >= str.length()) System.out.println("i:" + i + "  str:" + str);
         char[] chars = str.toCharArray();
-        if (chars[i]=='0'){
-            chars[i]='9';
-        }else {
+        if (chars[i] == '0') {
+            chars[i] = '9';
+        } else {
             chars[i]--;
         }
         return new String(chars);
+    }
+
+    //    bfs 双向搜索
+    public int openLock2(String[] deadends, String target) {
+        Set<String> deads = new HashSet<>();
+        deads.addAll(Arrays.asList(deadends));
+
+        Set<String> visited = new HashSet<>();
+
+//        一开始： set1是向下搜索  set2是向下搜索  的集合
+        Set<String> set1 = new HashSet<>();
+        set1.add("0000");
+        Set<String> set2 = new HashSet<>();
+        set2.add(target);
+
+        int step = 0;
+        //        ???TODO  为z么是两个集合都是得非空
+        while (!set1.isEmpty() && !set2.isEmpty()) {
+            Set<String> tmp = new HashSet<>();
+            Iterator<String> iterator = set1.iterator();
+            while (iterator.hasNext()) {
+                String cur = iterator.next();
+//
+                if (deads.contains(cur)) {
+                    continue;
+                }
+//                set1.contains(cur)&&set2.contains(cur)
+                if (set2.contains(cur)) {
+                    return step;
+                }
+//                将其余加入
+                for (int i = 0; i < 4; i++) {
+                    String up = upOne(cur, i);
+                    if (!visited.contains(up)) {
+                        tmp.add(up);
+                        visited.add(up);
+                    }
+                    String down = downOne(cur, i);
+                    if (!visited.contains(down)) {
+                        tmp.add(down);
+                        visited.add(down);
+                    }
+                }
+            }
+            step++;
+            set1 = set2;
+            set2 = tmp;
+        }
+        return -1;
+
     }
 }
