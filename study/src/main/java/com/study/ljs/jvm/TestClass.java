5b520d5ef15f3cd6af49c6f51bc9244561c22174
lijiasheng <lijiasheng@sinobest.cn>
2020-09-14 08:12:48 +0800

    【删除】删除idea

---
+++ study/src/main/java/com/study/ljs/jvm/TestClass.java
@@ -0,0 +1,13 @@
+package com.study.ljs.jvm;
+
+/**
+ * @author lijiasheng
+ * @version 1.0
+ * @date 2020/9/10 16:58
+ */
+public class TestClass {
+    private int m;
+    public int inc(){
+        return m+1;
+    }
+}
